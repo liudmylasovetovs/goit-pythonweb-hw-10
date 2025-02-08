4110b6cf1ead1ce9db323b9d429469806958838d
liudmylasovetovs <milkaegik@gmail.com>
2025-02-08 14:31:18 -0800

    fin fin commit refactoring smth

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from src.database.models import Base
-from src.conf.config import config as app_config
+from src.conf.config import settings as app_config
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
